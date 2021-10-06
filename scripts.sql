5156fb2d78b61ad1b0a6db5818eefd7e41806700
Agustina Di Tommaso <agustinaditommaso22@gmail.com>
2021-10-05 22:57:13 -0300

    Se crea archivo sql

--- scripts.sql
+++ scripts.sql
@@ -1,3 +1,2 @@
 CREATE TABLE `db_deportes`.`dp_jugador` ( `id_deportista` INT NOT NULL AUTO_INCREMENT , `nombre_apellido` VARCHAR(60) NOT NULL , `edad` INT(2) NOT NULL , `altura` INT(3) NOT NULL , `domicilio` VARCHAR(50) NOT NULL , `id_categoria` INT NOT NULL , PRIMARY KEY (`id_deportista`)) ENGINE = InnoDB;
-CREATE TABLE `db_deport
-CREATE TABLE `db_deportes`.`dp_usuario` ( `id_usuario` INT NOT NULL , `username` INT NOT NULL , `password` INT NOT NULL ) ENGINE = InnoDB;es`.`dp_categoria` ( `id_categoria` INT NOT NULL AUTO_INCREMENT , `descripcion` VARCHAR(300) NOT NULL , `tipo_competencia` VARCHAR NOT NULL , PRIMARY KEY (`id_categoria`)) ENGINE = InnoDB;
+CREATE TABLE `db_deportes`.`dp_categoria` ( `id_categoria` INT NOT NULL AUTO_INCREMENT , `descripcion` VARCHAR(300) NOT NULL , `tipo_competencia` VARCHAR NOT NULL , PRIMARY KEY (`id_categoria`)) ENGINE = InnoDB;
